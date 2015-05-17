f4926d94455d03192071270d7ed919d5eb54f8df
Rupal Khilari <rupal.khilari@gmail.com>
2015-05-18 03:55:36 +0530

    Addressed review comments. Used 2 space indentation to maintain consistency with the other files.

--- approval_polls/static/approval_polls/embed_instructions.js
+++ approval_polls/static/approval_polls/embed_instructions.js
@@ -1,26 +1,26 @@
 /* Code related to templates/approval_polls/embed_instructions.html */
 
 // Sets the iframe height and the corresponding embed instruction code in the textarea.
-function setIframeHeightAndCode(iframeId, textareaId, pollLink) {
+function setIframeHeightAndCode(iframeId, textareaId) {
 
-    // get the height of the contents of the iframe.
-    var iframeHeight = $(iframeId).contents().height();
+  var iframeHeight, pollLink;
 
-    // set the height of the iframe to that of its contents.
-    $(iframeId).height(iframeHeight);
+  // get the height of the contents of the iframe.
+  iframeHeight = $(iframeId).contents().height();
 
-    // write the HTML code into the textarea.
-    $(textareaId).val("<iframe src='"+pollLink+"' width='350px' height='"+iframeHeight+"px'></iframe>");
+  // set the height of the iframe to that of its contents.
+  $(iframeId).height(iframeHeight);
+
+  // obtain the src attribute of the iframe.
+  pollLink = $(iframeId).attr('src');
+
+  // write the HTML code into the textarea.
+  $(textareaId).val("<iframe src='" + pollLink + "' width='350px' height='" + iframeHeight + "px'></iframe>");
 }
 
 
 $(window).load(function() {
 
-    var iframeId = '#iframePreview';
-    var textareaId = '#textAreaCode';
-    var pollLink = $(iframeId).attr('src');
-
-    // Set the iframe height and textarea code.
-    setIframeHeightAndCode(iframeId, textareaId, pollLink);
+  setIframeHeightAndCode('#iframePreview', '#textAreaCode');
 });
 
