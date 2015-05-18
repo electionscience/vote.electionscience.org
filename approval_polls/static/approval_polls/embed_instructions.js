7824ecaf6ae7f83deee0adf145a7ed91664ec78c
Rupal Khilari <rupal.khilari@gmail.com>
2015-05-19 04:20:48 +0530

    Addressed comments. Wrapped the file in an IIFE.

--- approval_polls/static/approval_polls/embed_instructions.js
+++ approval_polls/static/approval_polls/embed_instructions.js
@@ -1,26 +1,21 @@
-/* Code related to templates/approval_polls/embed_instructions.html */
+(function() {
+  var setIframeHeightAndCode;
 
-// Sets the iframe height and the corresponding embed instruction code in the textarea.
-function setIframeHeightAndCode(iframeId, textareaId) {
+  /* Sets the iframe height and the corresponding embed instruction code in the
+     textarea. */
+  setIframeHeightAndCode = function(iframeId, textareaId) {
+    var iframeHeight, pollLink;
+ 
+    iframeHeight = $(iframeId).contents().height();
+    $(iframeId).height(iframeHeight);
+    pollLink = $(iframeId).attr('src');
 
-  var iframeHeight, pollLink;
+    $(textareaId).val("<iframe src='" + pollLink + "' width='350px' height='" +
+      iframeHeight + "px'></iframe>");
+  }
 
-  // get the height of the contents of the iframe.
-  iframeHeight = $(iframeId).contents().height();
+  $(window).load(function() {
+    setIframeHeightAndCode('#iframePreview', '#textAreaCode');
+  });
 
-  // set the height of the iframe to that of its contents.
-  $(iframeId).height(iframeHeight);
-
-  // obtain the src attribute of the iframe.
-  pollLink = $(iframeId).attr('src');
-
-  // write the HTML code into the textarea.
-  $(textareaId).val("<iframe src='" + pollLink + "' width='350px' height='" + iframeHeight + "px'></iframe>");
-}
-
-
-$(window).load(function() {
-
-  setIframeHeightAndCode('#iframePreview', '#textAreaCode');
-});
-
+}());
