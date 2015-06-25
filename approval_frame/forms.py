e4b6b8c5d7a6b2461a54f6a698aedd6a831e6b67
Rupal Khilari <rupal.khilari@gmail.com>
2015-06-25 10:43:39 -0700

    Added password reset functionality. https://github.com/electology/approval_frame/issues/48

--- approval_frame/forms.py
+++ approval_frame/forms.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from registration.forms import RegistrationFormUniqueEmail
+from django.contrib.auth.forms import PasswordResetForm
 
 class RegistrationFormCustom(RegistrationFormUniqueEmail):
     """
@@ -17,3 +18,22 @@ class RegistrationFormCustom(RegistrationFormUniqueEmail):
         error_messages={'invalid': _("This value may contain only letters, "
             + "numbers and ./+/-/_ characters.")}
     )
+
+class PasswordResetFormCustom(PasswordResetForm):
+    """
+    This class includes password reset form related, application-
+    specific customizations.
+
+    """
+    def clean_email(self):
+        # Make sure that the email address entered is associated with
+        # a registered, active user.
+        email = self.cleaned_data["email"]
+        try:
+            next(self.get_users(email))
+        except StopIteration:
+            raise forms.ValidationError(
+                _("This email address is not registered with us or " + 
+                "belongs to an account that hasn't been activated.")
+            )
+        return email
--- approval_frame/urls.py
+++ approval_frame/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from registration.forms import RegistrationFormUniqueEmail
 from registration.backends.default.views import RegistrationView
-from forms import RegistrationFormCustom
+from forms import RegistrationFormCustom, PasswordResetFormCustom
+from django.contrib.auth.views import password_reset, password_reset_done
 
 # autodiscover is required only for older versions of Django
 admin.autodiscover()
@@ -11,5 +12,8 @@ urlpatterns = patterns('',
     url(r'^approval_polls/', include('approval_polls.urls', namespace="approval_polls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationFormCustom), name='registration_register'),
+    url(r'^accounts/password/reset/$', password_reset, {'password_reset_form' : PasswordResetFormCustom}),
+    url(r'^accounts/password/reset/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^accounts/', include('registration.auth_urls')),
     url(r'^accounts/', include('registration.backends.default.urls'))
 )
