a6b5d8414fd41bb393c25d71bd6e3fb64d133cdc
Rupal Khilari <rupal.khilari@gmail.com>
2015-07-09 17:40:18 -0700

    Removed registered email address validation (due to privacy concerns)

--- approval_frame/forms.py
+++ approval_frame/forms.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from registration.forms import RegistrationFormUniqueEmail
-from django.contrib.auth.forms import PasswordResetForm
 
 class RegistrationFormCustom(RegistrationFormUniqueEmail):
     """
@@ -19,21 +18,3 @@ class RegistrationFormCustom(RegistrationFormUniqueEmail):
             + "numbers and ./+/-/_ characters.")}
     )
 
-class PasswordResetFormCustom(PasswordResetForm):
-    """
-    This class includes password reset form related, application-
-    specific customizations.
-
-    """
-    def clean_email(self):
-        # Make sure that the email address entered is associated with
-        # a registered, active user.
-        email = self.cleaned_data["email"]
-        try:
-            next(self.get_users(email))
-        except StopIteration:
-            raise forms.ValidationError(
-                _("This email address is not registered with us or " + 
-                "belongs to an account that hasn't been activated.")
-            )
-        return email
