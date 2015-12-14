6c992dca7ca97d87a2be395ba0d0e65174900a3e
kushankr <kushankr@umich.edu>
2015-12-13 19:38:24 -0500

    Issue 80 - Allow user to copy html code
    Created a 'Copy to Clipboard' button to copy HTML Code snippet in
    'embed_instructions'  page to Clipboard.
    Show a message depending on success or failure.

--- approval_polls/static/approval_polls/embed_instructions.js
+++ approval_polls/static/approval_polls/embed_instructions.js
@@ -19,3 +19,32 @@
   });
 
 }());
+
+$(document).ready(function() {
+  $("#copyText").click(function(){
+
+    /* Copy the HTML Code in textarea to ClipBoard */
+
+    var copyTextarea = document.querySelector('#textAreaCode');
+    copyTextarea.select();
+
+    try {
+      var successful = document.execCommand('copy');
+      if ($("#alert-success").length == 0)
+      {
+        $("<div id = 'alert-success' class='alert alert-success'>"
+          +"<strong>Success!</strong> Text copied to clipboard!.</div>")
+        .insertAfter($("#copyText"));
+      }      
+    } 
+    catch (err) {
+      if ($("#alert-danger").length == 0)
+      {
+        $("<div id = 'alert-danger' class='alert alert-danger'>"
+          +"<strong>Oops, unable to copy!</strong>"
+          +" To copy the text to clipboard: Ctrl+C!.</div>")
+        .insertAfter($("#copyText"));
+      }
+    }
+  });
+});
