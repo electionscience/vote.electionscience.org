82639221deb1780da5090c64c401e3df34d9c667
Balaji Subramanian <balajisubr@gmail.com>
2017-04-13 18:22:12 -0500

    Adding user subscriptions for logged in user

--- approval_frame/views.py
+++ approval_frame/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 
 from forms import NewUsernameForm
 from forms import RegistrationFormCustom
+from forms import ManageSubscriptionsForm
 from registration.backends.default.views import RegistrationView
-
+from approval_polls.models import Subscription
 from mailchimp_api import get_mailchimp_api
 import mailchimp
 
@@ -42,6 +43,46 @@ def changeUsernameDone(request):
         {'new_username': request.user}
     )
 
+@login_required
+def manageSubscriptions(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ManageSubscriptionsForm(request.POST)
+        if form.is_valid():
+            zipcode = form.cleaned_data.get('zipcode')
+            newslettercheckbox = form.cleaned_data.get('newslettercheckbox') 
+            if current_user.subscription_set.count() > 0:
+                if not newslettercheckbox and not zipcode:
+                    current_user.subscription_set.first().delete()
+            else:
+                subscr = Subscription(user=current_user, zipcode=zipcode)
+                subscr.save() 
+            return HttpResponseRedirect(
+                '/accounts/subscription/change/done/'
+                )
+    else:
+        if current_user.subscription_set.count() > 0:
+            subscr = current_user.subscription_set.first()
+            form = ManageSubscriptionsForm(initial={'user':request.user,'zipcode':subscr.zipcode}) 
+            boxchecked = True
+        else:
+            form = ManageSubscriptionsForm()
+            boxchecked = False
+
+        return render(
+            request,
+            'registration/subscription_preferences.html',
+            {'form': form,'boxchecked':boxchecked}
+        )     
+
+
+@login_required
+def manageSubscriptionsDone(request):
+    return render(
+        request,
+        'registration/subscription_change_done.html',
+        {'new_username': request.user}
+    )
 
 class CustomRegistrationView(RegistrationView):
     form_class = RegistrationFormCustom
