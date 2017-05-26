66bbea25783656ccb45afa372c0da74713894f76
Balaji Subramanian <balajisubr@gmail.com>
2017-05-25 23:05:22 -0500

    Updating cron for updating user subscriptions

--- approval_frame/mailchimp_api.py
+++ approval_frame/mailchimp_api.py
@@ -4,14 +4,12 @@ from approval_polls.models import Subscription
 from registration.signals import user_registered
 from django.dispatch import receiver
 
-
 def get_mailchimp_api():
     if settings.MAILCHIMP_API_KEY:
         key = settings.MAILCHIMP_API_KEY
     else:
         key = '00000000000000000000000000000000-us1'
     return mailchimp.Mailchimp(key)
-
 
 def update_subscription(subscription_preference, email, zipcode):
     try:
@@ -36,24 +34,3 @@ def receive_new_user_registered(sender, user, request, **kwargs):
     if 'newslettercheckbox' in request.POST:
         subscr = Subscription(user=user, zipcode=request.POST['zipcode'])
         subscr.save()
-
-def update_user_subscriptions():
-    m = get_mailchimp_api()
-    lists = m.lists.list()
-    list_id = lists['data'][0]['id']
-    members = m.lists.members(list_id)['data']
-    subscriptions_local = Subscription.objects.all()
-    for sub in subscriptions_local:
-        user = sub.user
-        local_email = user.email 
-        user_present = False
-        for member in members:
-            if member['email'] == local_email:
-                user_present = True
-                break
-        if user_present == False:
-            user.subscription_set.first().delete()
-
-if __name__ == "__main__":
-    update_user_subscriptions()
-
