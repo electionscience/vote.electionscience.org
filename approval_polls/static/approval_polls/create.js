6c3f37808e79fe4d587842371c9d4b623e7a9560
Blake Curry <blake.curry@gmx.us>
2017-06-15 15:58:00 -0400

    Removed number identifier from choice placeholder text.

--- approval_polls/static/approval_polls/create.js
+++ approval_polls/static/approval_polls/create.js
@@ -11,8 +11,8 @@ $(function () {
     this.numChoiceFields++;
     formGroup = $("<div class='form-group'></div>");
     input = $("<div class='input-group' id='div-choice" + this.numChoiceFields + "'><input class='form-control' type='text' maxlength=200 name='choice" +
-            this.numChoiceFields + "' placeholder='Choice " +
-            this.numChoiceFields + "'><span class='input-group-addon'>"+
+            this.numChoiceFields + "' placeholder='Choice'" +
+            "'><span class='input-group-addon'>"+
             "<a href='#' class='add-link' id='link-choice" + this.numChoiceFields + "' title='Add link' data-toggle='tooltip' data-placement='bottom'>"+
             "<span class='glyphicon glyphicon-link'></span></a></span>" +
             "<span class='input-group-addon'>"+
