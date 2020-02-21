6d3e16a4ec585aa329e2ebb5d063c05990b032c6
Rupal Khilari <rupal.khilari@themill.com>
2020-02-20 21:15:57 -0800

    Add action to export voter opt-in emails.

--- approval_polls/admin.py
+++ approval_polls/admin.py
@@ -1,4 +1,7 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from approval_polls.models import Ballot, Choice, Poll
 
@@ -25,6 +28,45 @@ class PollAdmin(admin.ModelAdmin):
     list_display = ('question', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['question']
+    actions = ["export_voters_as_csv"]
+
+    def export_voters_as_csv(self, request, queryset):
+        poll_field_names = [
+            'id', 'question', 'pub_date', 'user', 'vtype', 'show_email_opt_in',
+        ]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = (
+            'attachment; filename=voter_emails.csv'
+        )
+        writer = csv.writer(response)
+
+        for poll in queryset:
+            writer.writerow([])
+            writer.writerow(poll_field_names)
+            writer.writerow(
+                [getattr(poll, field) for field in poll_field_names]
+            )
+            writer.writerow([])
+            for ballot in poll.ballot_set.all():
+                ballot_field_names = [
+                    'username', 'first_name', 'last_name', 'email'
+                ]
+                writer.writerow(ballot_field_names)
+                if ballot.user:
+                    writer.writerow(
+                        [getattr(
+                            ballot.user, field
+                        ) for field in ballot_field_names]
+                    )
+                else:
+                    writer.writerow(['', '', '', ballot.email])
+
+        return response
+
+    export_voters_as_csv.short_description = (
+        'Export poll with opt-in voter emails.'
+    )
 
 
 admin.site.register(Poll, PollAdmin)
