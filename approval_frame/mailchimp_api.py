5253750624b4189e26ceb018557b9ae3ee3b359c
Felix Sargent <felix@truework.com>
2022-02-23 12:54:03 -0800

    Move to Bootstrap theme"

--- approval_frame/mailchimp_api.py
+++ approval_frame/mailchimp_api.py
@@ -24,11 +24,15 @@ def update_subscription(subscription_preference, email, zipcode):
         else:
             m.lists.unsubscribe(list_id, {'email': email}, {'MMERGE3': zipcode})
     except mailchimp.ListAlreadySubscribedError:
-        errors.append('newslettercheckbox')
-        errors.append('That email is already subscribed to the list')
+        errors.extend(
+            (
+                'newslettercheckbox',
+                'That email is already subscribed to the list',
+            )
+        )
+
     except mailchimp.Error as e:
-        errors.append('newslettercheckbox')
-        errors.append(e)
+        errors.extend(('newslettercheckbox', e))
     return errors
 
 
