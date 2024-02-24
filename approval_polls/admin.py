611e363066d7bee36313671b06818fa6792cf9b9
Felix Sargent <fsargent@recruitbot.com>
2024-02-24 15:11:08 -0800

    Fix trunk

--- approval_polls/admin.py
+++ approval_polls/admin.py
@@ -1,7 +1,6 @@
 import csv
 
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from import_export import resources
